6d845d7434866665ccd139d0915a7d8b5fde895e
igbanibo-Dengi <igbanibodengi@gmail.com>
2024-11-02 20:12:23 +0100

    ui update

--- components/ContentCategories.tsx
+++ components/ContentCategories.tsx
@@ -7,19 +7,19 @@ export function ContentCategories() {
             <h2 className="text-4x md:text-5xl 2xl:text-6xll font-bold mb-8 text-center">Productions</h2>
             <div className="container mx-auto px-4">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                    <div className="bg-background border-4 border-[#C1A355] border-dashed p-8 rounded-lg text-center">
-                        <Music className="w-16 h-16 mx-auto mb-4" />
+                    <div className="bg-background border-4 border-white hover:border-[#C1A355] group border-dashed p-8 rounded-lg text-center">
+                        <Music className="w-16 h-16 mx-auto mb-4 group-hover:scale-125 group-hover:text-[#C1A355] text-white transition-all duration-500 ease-in-out" />
                         <h3 className="text-2xl font-bold mb-2">Music Label</h3>
                         <p className="mb-4">Discover our talented artists and their latest tracks</p>
-                        <Button variant="outline" className="text-white border-white hover:bg-white hover:text-purple-900">
+                        <Button variant="outline" className="border-white group-hover:border-[#C1A355] group-hover:text-[#C1A355]">
                             Explore Music
                         </Button>
                     </div>
-                    <div className="bg-background border-4 border-white border-dashed p-8 rounded-lg text-center">
-                        <Video className="w-16 h-16 mx-auto mb-4" />
+                    <div className="bg-background border-4 border-white hover:border-[#C1A355] group border-dashed p-8 rounded-lg text-center">
+                        <Video className="w-16 h-16 mx-auto mb-4 group-hover:scale-125 group-hover:text-[#C1A355] text-white transition-all duration-500 ease-in-out" />
                         <h3 className="text-2xl font-bold mb-2">Comedy Content</h3>
                         <p className="mb-4">Laugh out loud with our hilarious skits and shows</p>
-                        <Button variant="outline" className="text-white border-white hover:bg-white hover:text-blue-900">
+                        <Button variant="outline" className="border-white group-hover:border-[#C1A355] group-hover:text-[#C1A355]">
                             Watch Comedy
                         </Button>
                     </div>
